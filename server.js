7cad72f6973ec41e809f2f252ff973d5749e32c8
Alan-Bi <alan.bi326@gmail.com>
2019-05-02 21:08:58 -0700

    Attempt again

--- server.js
+++ server.js
@@ -11,12 +11,16 @@ app.get('/', function(req, res) {
 });
 
 // Add this for Let's Encrypt ACME challenge validation
-app.get('/.well-known/acme-challenge/:str', function (req, res) {
-  res.send(req['str']);
+app.get('/.well-known/acme-challenge/11eWXlp0ahTsFPHpFRh73dpnn2GOtGlAjKTpDcoXLmI', function (req, res) {
+  res.send('11eWXlp0ahTsFPHpFRh73dpnn2GOtGlAjKTpDcoXLmI.o8Wi9GTAN0qG_ckoj-thcH8VFvxSyQI5UhvbqAWKVck');
+})
+app.get('/.well-known/acme-challenge/hlFBUsiFNuKF_UDD6psPkX3_FBAX85swt7Pi2gFw1ho', function (req, res) {
+  res.send('hlFBUsiFNuKF_UDD6psPkX3_FBAX85swt7Pi2gFw1ho.o8Wi9GTAN0qG_ckoj-thcH8VFvxSyQI5UhvbqAWKVck');
 })
 
 /* static files */
 app.use(favicon(path.join(__dirname, '/favicon.ico')));
 app.use("/css", express.static(__dirname + '/css'));
+// app.use("test", express.static(__dirname + '/.well-known'));
 
 app.listen(port);
