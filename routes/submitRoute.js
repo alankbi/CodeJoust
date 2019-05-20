f3424e772b9404f39046d83fe0cc9f7fbceba277
Alan-Bi <alan.bi326@gmail.com>
2019-05-20 08:47:35 -0700

    Add route to submit to Judge0

---
+++ routes/submitRoute.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const request = require('request');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const { code, languageId } = req.body;
+  const options = {
+    url: 'https://api.judge0.com/submissions',
+    form: {
+      source_code: code,
+      language_id: languageId,
+      wait: true,
+    },
+  };
+
+  request.post(options, (error, response, body) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(body);
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const submitRoute = require('./routes/submitRoute');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Example API route
 app.get('/api/hello/', async (req, res) => {
   res.json({ text: 'Hello World!' });
 });
+
+// Load in API routes for submitting
+app.use('/api/submit/', submitRoute);
 
 // React production files
 app.use(express.static(path.join(__dirname, 'client/build')));
